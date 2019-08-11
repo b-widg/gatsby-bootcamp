8b5448a5dc4a2ee3e31b44cfb740eac1bdbd0fbe
b-widg <b-widg@users.noreply.github.com>
2019-08-11 09:28:24 -0700

    adding pages

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div className="about-wrapper">
+      <h1 className="title">About Title</h1>
+      <p>Here is a bit about me</p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ContactPage = () => {
+  return (
+    <div className="contact-wrapper">
+      <h1 className="title">My Contact Info</h1>
+      <div className="contact-details">
+        Here are some contact details. Please get in touch.
+      </div>
+    </div>
+  );
+};
+
+export default ContactPage;
